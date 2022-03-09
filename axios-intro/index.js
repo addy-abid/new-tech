ea5cc1f28ab913c0fd3ad064b78cb3c3e0d614dd
Addy Abid <addy.abeed@gmail.com>
2022-03-09 03:31:06 +0000

    added eslint comment to disable console.log error

--- axios-intro/index.js
+++ axios-intro/index.js
@@ -2,5 +2,6 @@ const axios = require('axios').default;
 
 axios.get('https://pokeapi.co/api/v2/pokemon/cubone')
   .then(result => {
+    // eslint-disable-next-line no-console
     console.log(result);
   });
