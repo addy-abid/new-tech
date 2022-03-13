88228659a45b2947127d5468174e620608bff650
Addy Abid <addy.abeed@gmail.com>
2022-03-13 01:17:02 +0000

    3/5 tests passing - trying mocha to view tests

---
+++ algos-consecutive-array/index.js
@@ -0,0 +1,25 @@
+/**
+ * make array consecutive
+ * for array, const arr =  [1, 2, 5], find how many integers are needed
+ * to make numbers consecutive to the highest number
+ *
+ * steps:
+ * set all integers in numerical order
+ * look at each value in array
+ * if int after current value = current value + 1,
+ * continue
+ * otherwise,
+ * add 1
+ */
+
+const makeArrayConsecutive = array => {
+  const orderedArray = array.sort();
+  var count = 0;
+  for (let i = 0; i < orderedArray.length; i++) {
+    if (orderedArray[i + 1] !== orderedArray[i] + 1) {
+      count++;
+    }
+  }
+  return count;
+};
+makeArrayConsecutive();
