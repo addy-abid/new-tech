30fd87a2cdcf8d4a52937a390e15bb5da97b2f9d
Addy Abid <addy.abeed@gmail.com>
2022-08-01 20:26:35 +0000

    merging master

--- axios-intro/index.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios').default;
-
-axios.get('https://pokeapi.co/api/v2/pokemon/cubone')
-  .then(result => {
-    // eslint-disable-next-line no-console
-    console.log('result:', result);
-  });
